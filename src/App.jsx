d1c9cef0e5b229cd7c8c043e8279990df4bc1630
Park jin-a <jiiinap@naver.com>
2026-05-06 20:16:42 +0900

    item 저장하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react"; // 생략 가능
 import NameInput from "./NameInput";
 import HookStructure from "./HookStructure";
+import ItemList from "./ItemList";
 
 const App = () => {
 	return (
 		<div>
-			<HookStructure />
+			<ItemList />
 		</div>
 	);
 };
