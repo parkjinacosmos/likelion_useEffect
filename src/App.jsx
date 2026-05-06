63bb5a96b8fa12f08151ee76662c9257d9765c4b
Park jin-a <jiiinap@naver.com>
2026-05-06 20:40:43 +0900

    영화 목로 ㄱ가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import NameInput from "./NameInput";
 import HookStructure from "./HookStructure";
 import ItemList from "./ItemList";
 import TimerSwitch from "./TimerSwitch";
+import MovieList from "./MovieList";
 
 
 
 const App = () => {
 	return (
 		<div>
-			<TimerSwitch />
+			<MovieList />
 		</div>
 	);
 };
